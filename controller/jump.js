5584f1a26ddac7157f862986cbdf9f3dcd452753
Tyly04 <tyly04@gmail.com>
2018-07-11 23:20:09 -0700

    Controls

--- controller/jump.js
+++ controller/jump.js
@@ -23,6 +23,10 @@ function parseDat(data){
 }
 var BTSP = require('bluetooth-serial-port');
 var serial = new BTSP.BluetoothSerialPort();
+var prevState = null;
+var cX = null;
+var cY = null;
+var cZ = null;
 serial.on('found', function(address, name){
   if(name.indexOf('DSD TECH') != -1){
     console.log(address);
@@ -32,8 +36,50 @@ serial.on('found', function(address, name){
         console.log('connected');
         serial.on('data', function(buffer){
           var dat = buffer.toString('utf8');
-          console.log(parseDat(dat));
-
+          var data = parseDat(dat);
+          console.log(data);
+            UpdateStomp.inputState = 0;
+            if(data){
+              //Configuration:
+              var x = parseInt(data.input[0]);
+              var y = parseInt(data.input[1]);
+              var z = parseInt(data.input[2]);
+              //Should be how this is configured:
+              //(Accounting for accelerometer rotation)
+              x = z * 1;
+              y = y * 1;
+              z = x * 1;
+              if(cX){
+                if(cZ > Math.abs(z) && Math.abs(z) + 3 < Math.abs(cZ)){
+                  //STOMP
+                  if(prevState !== null){
+                    execute(__dirname + prevState + ".exe");
+                  }
+                }
+                prevState = null;
+                if(Math.abs(cY) > Math.abs(y) && Math.abs(y) + 3 < Math.abs(cY)){
+                  //LEFT
+                  prevState = "a";
+                } else if (Math.abs(cY) < Math.abs(y) && Math.abs(y) - 3 > Math.abs(cY)){
+                  //RIGHT
+                  prevState = "d";
+                }
+                if(Math.abs(cX) > Math.abs(x) && Math.abs(x) + 3 < Math.abs(cX)){
+                  //UP
+                  prevState = "w";
+                } else if (Math.abs(cX) < Math.abs(x) && Math.abs(x) - 3 > Math.abs(cX)){
+                  //DOWN
+                  prevState = "s";
+                }
+                cX = x;
+                cY = y;
+                cZ = z;
+              } else {
+                cX = x;
+                cY = y;
+                cZ = z;
+              }
+            }
         });
       }, function(){
         console.log('cannot connect');
